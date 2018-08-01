31797b95ba901ba4c83f57905d863c6e2c2f8727
Sergey Karabuta <siriusitsastar@gmail.com>
2018-08-02 00:59:28 +0300

    HomeWork1

--- Project1/src/CarOwner.java
+++ Project1/src/CarOwner.java
@@ -4,12 +4,20 @@ public class CarOwner {
 	public static void main(String[] args) {
 		
 		int distance;
+		int time=20;
 		
 		Car myCar = new Car();
 		myCar.start();
-		distance = myCar.drive(20);
-		System.out.println("Моя машина проехала "+distance+" километров!!");
+		distance = myCar.drive(time);
+		System.out.println("Моя машина проехала "+distance+" километров за "+time+" часов");
 		myCar.stop();
+		
+		JamesBondCar jbCar = new JamesBondCar();
+		jbCar.start();
+		distance = jbCar.drive(time);
+		System.out.println("Машина 007 проехала "+distance+" километров за "+time+" часов");
+		jbCar.stop();
+		
 	}
 
 }
---
+++ Project1/src/JamesBondCar.java
@@ -0,0 +1,8 @@
+
+public class JamesBondCar extends Car {
+	public int drive(int howlong) {
+		int distance = 0;
+		distance = howlong*180;
+		return distance;
+	}
+}
